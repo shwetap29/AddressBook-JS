2187c7ecee90cda75c99210235a57830a299d653
shwetap29 <shwetaprasad48@gmail.com>
2021-07-06 15:15:44 +0530

    Welcome to Address book problem

---
+++ UC1_CreateContacts.js
@@ -0,0 +1,20 @@
+//Creating Address book contacts
+class AddressBook {
+    constructor(...params) {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
+    }
+
+    toString() {
+        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email;
+    }
+}
+
+let addressBook = new AddressBook("Shweta", "Prasad", "Thane", "Mumbai", "Mhahrashtra", "411057", "7894562103", "shwetaprasad48@gmail.com");
+console.log(addressBook.toString());
